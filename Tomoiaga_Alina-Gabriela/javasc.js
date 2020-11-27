6e1584e4dd461a5199a61615a0a3eed4b3772b0a
Alina117 <73290833+Alina117@users.noreply.github.com>
2020-11-27 18:59:55 +0200

    Add files via upload

---
+++ Tomoiaga_Alina-Gabriela/javasc.js
@@ -0,0 +1,19 @@
+var slideIndex = 0;
+slideshow();
+
+function slideshow() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var radio = document.getElementsByClassName("radio");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  
+  if (slideIndex > slides.length) {slideIndex = 1} 
+ 
+  slides[slideIndex-1].style.display = "block";  
+  radio[slideIndex-1].className += " active";
+  setTimeout(slideshow, 3000);
+}
